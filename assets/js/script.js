ab2592389e32c86896fb6911b7a2f776e17db295
Kat Yeary <katyeary@gmail.com>
2020-05-22 13:47:25 -0700

    adjust comments, dry out text a little. Local Storage can't reliably store content in the order it's set in. Trying to figure out a jquery cookie workaround that won't require node.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
-    //on opening, load the most recent item in the search history
+
+    //display the most recent search in search history on document load
+    var history = Object.keys(localStorage);
+    //var history = arr.reverse();
+    getCoords(history[0]);
+    searchHistory();
+
 
     //Find and render the date and time
     var dateObj = new Date();
@@ -20,14 +26,16 @@ $(document).ready(function () {
     // This function handles the search-button click event
     $("#search-city").on("click", function (event) {
         event.preventDefault();
+        $(".search-history").empty();
+        searchHistory();
         // This line grabs the input from the textbox
         var city = $("#search-input").val().trim();
-        getID(city);
+        getCoords(city);
     });
 
 
-    //Make a call to retrieve city ID
-    function getID(city) {
+    //Make a call for geographic coordinates - coordinates are required to access OWM's "One Call" JSON object. 
+    function getCoords(city) {
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=4081942e5f99ae6e3a9f08ab5e60f5ca";
 
         $.ajax({
@@ -46,9 +54,9 @@ $(document).ready(function () {
         });
     }
 
-
+    //Get the "One Call" object
     function oneCall(city) {
-        //get the geographic coordinates stored in the previous call
+        //get the geographic coordinates from local storage
         var coords = JSON.parse(localStorage.getItem(city));
         var lat = coords.lat;
         var lon = coords.lon;
@@ -58,6 +66,7 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
+            //remove existing forecast content from browser
             $(".forecast").empty();
             var current = response.current;
             var forecast = response.daily;
@@ -68,7 +77,7 @@ $(document).ready(function () {
             var weather = current.weather[0].description;
             var icon = current.weather[0].icon;
 
-
+            //create an object that holds all the necessary data
             var weatherData = {
                 humidity,
                 icon,
@@ -81,10 +90,10 @@ $(document).ready(function () {
                 forecast
             };
 
-            //save all data in local storage by city name
+            //save all data in local storage by city
             localStorage.setItem(city, JSON.stringify(weatherData));
-            //render the data to the browser
-           
+
+            //Render the weather conditions of the most recent city search
             renderData(city);
 
         });
@@ -92,9 +101,9 @@ $(document).ready(function () {
 
 
 
-    //function to render data to page
+    //Function to render data of the most recent/current city-search to the browser
     function renderData(city) {
-        $(".city").html("<h1>" + city + " Weather Details</h1>");
+        //get weatherData of most recent city search from local storage
         var data = JSON.parse(localStorage.getItem(city));
         var humidity = data.humidity;
         var icon = data.icon;
@@ -102,14 +111,36 @@ $(document).ready(function () {
         var uvi = data.uvi;
         var weather = data.weather;
         var wind = data.wind;
+        
+        //render weather data to browser
+        $(".city").html("<h1>" + city + " Weather Details</h1>");
         $(".wind").text("Wind Speed: " + wind);
         $(".icon").html("<img src='https://openweathermap.org/img/w/" + icon + ".png'>");
         $(".humidity").text("Humidity: " + humidity);
         $(".temp").text("Temperature (F) " + temp.toFixed(2));
-        $(".uv-index").text("UV Index: " + uvi);
-        $(".uv-index").css("display", "block");
+        //remove quotation marks before rendering
         $(".weather").text("Conditions: " + weather.replace(/\"/g, ""));
- 
+
+        $(".uv-index").text("UV Index: " + uvi);
+        //given the uv index for the search item, display a color that indicates uv-index conditions
+        if (uvi > 8) {
+            //very severe conditions
+            $(".uv-index").css("background-color", "purple");
+        } else if (uvi > 5) {
+            //severe conditions
+            $(".uv-index").css("background-color", "red");
+        } else if (uvi > 3) {
+            //moderate conditions
+            $(".uv-index").css("background-color", "yellow");
+        } else if (uvi < 3) {
+            //favorable conditions
+            $(".uv-index").css("background-color", "green");
+        }
+        //make the uv-index block visible
+        $(".uv-index").css("display", "block");
+
+        //One Call's default behavior is to retrieve the forecast of the following 7 days. 
+        //Create an object that retrieves date, temperature, icon and humidity for only the next five days
         var fiveDay = {
             dayOne: data.forecast[1],
             dayTwo: data.forecast[2],
@@ -117,42 +148,61 @@ $(document).ready(function () {
             dayFour: data.forecast[4],
             dayFive: data.forecast[5],
         }
- 
-    
+        
+        //render the data for each key-value of the 5-day forecast object 
         $.each(fiveDay, function (index) {
             var forecast = fiveDay[index];
             var humidityF = forecast.humidity;
             var tempF = (forecast.temp.max - 273.15) * 1.80 + 32;
             var iconF = (forecast.weather[0].icon);
             //  var dt = forecast.dt;
-
+   
             $(".forecast").append("<p class='" + index + "'>");
             $("." + index).html("<p><img src='https://openweathermap.org/img/w/" + iconF + ".png'></p>");
-            $("." + index).append("<p id='" + index + "-date'>");
+          //  $("." + index).append("<p id='" + index + "-date'>");
             $("." + index).append("<p id='" + index + "-temp'>");
             $("." + index).append("<p id='" + index + "-humidity'>");
 
-            //     $("#" + index + "-date").text();
             $("#" + index + "-temp").text("Temperature (F) " + tempF.toFixed(2));
             $("#" + index + "-humidity").text("Humidity: " + humidityF);
-
         });
 
     }
 
-    var history = Object.keys(localStorage);
-    console.log(history);
-    for (let i = 0; i < 5; i++) {
-        var storedCity = history[i];
-        $(".search-history").append("<button>" + storedCity + "</button>");
-        console.log(storedCity);
-    } 
-       
-       
-   
-     //   console.log(storedCity);
-     //    $(".search-history").append("<button id='" + storedCity + "'>")
-      //   $("#" + storedCity).text(storedCity);
-    
+//Loop through 5 most recent items in local storage
+    function searchHistory() {
+        if (history.length >= 5) {
+            //if the local storage contains more than 5 keys, retrieve data for indexes 1-5
+            for (let i = 0; i < 5; i++) {
+                var storedCity = history[i];
+                //make a button for each item 
+                selectHistory(storedCity);
+            }
+        } else if (history.length < 5) {
+            //if there are less than five items in local storage, only create buttons for existing items
+            //otherwise, you'll get "empty" buttons
+            for (let i = 0; i < history.length; i++) {
+                var storedCity = history[i];
+                selectHistory(storedCity);
+            }
+        } else if (history = []) {
+            //if there is nothing in local storage, only display the search bar
+            $("main").css("display", "none");
+        }
+
+    }
+
+//Function to create buttons and click event for the stored city-searches. Buttons pull up current weather and forecast for that city when clicked.   
+    function selectHistory(storedCity) {
+        //remove spaces from the storedCity string and create ID for buttons
+        var searchID = storedCity.replace(/ /g,'');
+        //dynamically append html buttons
+        $(".search-history").append("<button id='" + searchID + "'>" + storedCity + "</button>");
+        $("#" + searchID).on('click', function (event) {
+            event.preventDefault();
+            getCoords(storedCity);
+        });
+    }
+
 });
 
